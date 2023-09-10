69f5f38d2edda57487e324615681aefa7b5e5068
WillSchmidt97 <willschmidt1997@gmail.com>
2023-09-09 21:43:05 -0300

    CRUD finished!

--- ContactControl/Controllers/ContactsController.cs
+++ ContactControl/Controllers/ContactsController.cs
@@ -39,21 +39,64 @@ namespace ContactControl.Controllers
         }
         public IActionResult DeleteConfirmed(int id)
         {
-            _contactRepo.DeleteConfirmed(id);
-            return RedirectToAction("Index", "Home");
+            try
+            {
+                bool deleted = _contactRepo.DeleteConfirmed(id);
+                if (!deleted) 
+                {
+                    TempData["SuccessMessage"] = "It was not possible deleting this contact.";
+                }
+                else
+                {
+                    TempData["SuccessMessage"] = "Contact deleted successfully!";
+                }
+                return RedirectToAction("Index", "Home");
+            }
+            catch(Exception ex)
+            {
+                TempData["ErrorMessage"] = $"Something went wrong: {ex.Message}";
+                return RedirectToAction("Index");
+            }
         }
         [HttpPost]
         public IActionResult AddCont(ContactsModel contacts)
         {
-            _contactRepo.Adicionar(contacts);
-            return RedirectToAction("Index");
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _contactRepo.Adicionar(contacts);
+                    TempData["SuccessMessage"] = "Contact added successfully!";
+                    return RedirectToAction("Index");
+                }
+                return (View(contacts));
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = $"Something went wrong: {ex.Message}";
+                return RedirectToAction("Index");
+            }
         }
 
         [HttpPost]
         public IActionResult Change(ContactsModel contacts)
         {
-            _contactRepo.Att(contacts);
-            return RedirectToAction("Index");
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _contactRepo.Att(contacts);
+                    TempData["SuccessMessage"] = "Contact edited successfully!";
+                    return RedirectToAction("Index");
+                }
+
+                return (View("Edit", contacts));
+            }
+            catch(Exception ex)
+            {
+                TempData["ErrorMessage"] = $"Something went wrong: {ex.Message}";
+                return RedirectToAction("Index");
+            }
         }
 
     }
--- ContactControl/Models/ContactsModel.cs
+++ ContactControl/Models/ContactsModel.cs
@@ -1,15 +1,27 @@
-﻿namespace ContactControl.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactControl.Models
 {
     public class ContactsModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Name field required!")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Email field required!")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Phone field required!")]
+        [Phone(ErrorMessage = "Invalid phone number.")]
         public string Phone { get; set; }
+        [Required(ErrorMessage = "Postal code field required!")]
         public string Postalcode {get ; set; }
+        [Required(ErrorMessage = "State field required!")]
         public string State { get; set; }
+        [Required(ErrorMessage = "City field required!")]
         public string City { get; set; }
+        [Required(ErrorMessage = "Neighborhood field required!")]
         public string Neighborhood { get; set; }
+        [Required(ErrorMessage = "Address field required!")]
         public string Address { get; set; }
     }
 }
