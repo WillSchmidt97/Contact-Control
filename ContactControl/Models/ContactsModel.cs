96d6ca31267562af5b449f415941fe935b78727b
WillSchmidt97 <willschmidt1997@gmail.com>
2024-09-25 18:28:08 -0300

    modified error messages

--- ContactControl/Models/ContactsModel.cs
+++ ContactControl/Models/ContactsModel.cs
@@ -7,21 +7,21 @@ namespace ContactControl.Models
         public int Id { get; set; }
         [Required(ErrorMessage = "Name field required!")]
         public string Name { get; set; }
-        [Required(ErrorMessage = "Email field required!")]
-        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        [Required(ErrorMessage = "Email field required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address. Please, provide an existing email.")]
         public string Email { get; set; }
         [Required(ErrorMessage = "Phone field required!")]
         [Phone(ErrorMessage = "Invalid phone number.")]
         public string Phone { get; set; }
-        [Required(ErrorMessage = "Postal code field required!")]
+        [Required(ErrorMessage = "Postal code field required.")]
         public string Postalcode {get ; set; }
-        [Required(ErrorMessage = "State field required!")]
+        [Required(ErrorMessage = "State field required.")]
         public string State { get; set; }
-        [Required(ErrorMessage = "City field required!")]
+        [Required(ErrorMessage = "City field required.")]
         public string City { get; set; }
-        [Required(ErrorMessage = "Neighborhood field required!")]
+        [Required(ErrorMessage = "Neighborhood field required.")]
         public string Neighborhood { get; set; }
-        [Required(ErrorMessage = "Address field required!")]
+        [Required(ErrorMessage = "Address field required.")]
         public string Address { get; set; }
     }
 }
